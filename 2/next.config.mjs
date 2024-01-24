e5ce166f1213fafb5120bb6046f31581b00b5747
siwat jermwatthana <siwat.j@hiveground.com>
2024-01-24 15:32:06 +0700

    [Feat]: Add rewrites for backend API and static files

--- 2/next.config.mjs
+++ 2/next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        // for backend API
+        source: "/api/:path*",
+        destination: "http://localhost:3001/api/:path*",
+      },
+      {
+        // for backend static files (img png doc and other)
+        source: "/static/:path*",
+        destination: "http://localhost:3001/static/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
