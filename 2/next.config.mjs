881a6289923b018e8d18791198a1a5ea90ad1ea4
siwat jermwatthana <siwat.j@hiveground.com>
2024-01-25 23:58:37 +0700

    [Fix]: Add middleware and comment out rewrites in next.config

--- 2/next.config.mjs
+++ 2/next.config.mjs
@@ -1,19 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        // for backend API
-        source: "/api/:path*",
-        destination: "http://localhost:3001/api/:path*",
-      },
-      {
-        // for backend static files (img png doc and other)
-        source: "/static/:path*",
-        destination: "http://localhost:3001/static/:path*",
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       // for backend API
+  //       source: "/api/:path*",
+  //       destination: "http://localhost:3001/api/:path*",
+  //     },
+  //     {
+  //       // for backend static files (img png doc and other)
+  //       source: "/static/:path*",
+  //       destination: "http://localhost:3001/static/:path*",
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
