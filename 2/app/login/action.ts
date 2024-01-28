77b451fae6022f76b4a9371afd7938a6079cb3c3
siwat jermwatthana <siwat.j@hiveground.com>
2024-01-28 23:43:19 +0700

    [Feat]: Add auth and middleware functional

--- 2/app/login/action.ts
+++ 2/app/login/action.ts
@@ -1,13 +1,28 @@
 "use server";
 
-export const login = (prevState: any, formData: FormData) => {
+import { SignJWT, importJWK } from "jose";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+
+export const login = async (prevState: any, formData: FormData) => {
   const email = formData.get("email") as string;
   const password = formData.get("password") as string;
 
-  if (email !== "admin" && password !== "1234") {
+  if (email !== "admin" || password !== "1234") {
     return {
       ...prevState,
       message: "Invalid email or password",
     };
   }
+
+  const secretJWK = {
+    kty: "oct",
+    k: process.env.JWT_SECRET,
+  };
+
+  const secretKey = await importJWK(secretJWK, "HS256");
+  const token = await new SignJWT({ email }).setProtectedHeader({ alg: "HS256" }).setIssuedAt().setExpirationTime("2h").sign(secretKey);
+  cookies().set("token", token);
+
+  redirect("/blog");
 };
--- 2/middleware.ts
+++ 2/middleware.ts
@@ -1,14 +1,51 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { jwtVerify, importJWK } from "jose";
 
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  console.log("middleware next/server");
-  //   return NextResponse.redirect(new URL("/", request.url));
-  return NextResponse.next();
+export async function middleware(request: NextRequest) {
+  try {
+    const parsedUrl = new URL(request.url);
+    const pathname = parsedUrl.pathname.replace(/^(\/)/, "");
+    if (pathname === "content") {
+      return NextResponse.next();
+    }
+
+    // Middleware for route /blog
+    const token = request.cookies.get("token");
+    const tokenValue = token?.value;
+
+    if (!tokenValue) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+
+    const secretJWK = {
+      kty: "oct",
+      k: process.env.JWT_SECRET,
+    };
+    const secretKey = await importJWK(secretJWK, "HS256");
+    const { payload } = await jwtVerify(tokenValue, secretKey);
+
+    if (payload.email !== "admin") {
+      throw new Error("Email incorrect");
+    }
+
+    // Pass user data to headers
+    const requestHeaders = new Headers(request.headers);
+    requestHeaders.set("user", JSON.stringify({ email: payload.email }));
+    const response = NextResponse.next({
+      request: {
+        headers: requestHeaders,
+      },
+    });
+
+    return response;
+  } catch (error) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: "/content/:path*",
+  matcher: ["/content/:path*", "/blog/:path*"],
 };
